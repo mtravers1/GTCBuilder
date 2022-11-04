3c144907863efc945dab522db864925fb0d44c27
George, B. Smith <georgebsmith.tech@gmail.com>
2022-11-04 13:08:37 +0100

    Consumed site Logo and Name from the backend

--- constants/EndPoints.jsx
+++ constants/EndPoints.jsx
@@ -4,4 +4,5 @@ const siteDetailsBase = `/${base}/site-details`;
 export const siteDetails = {
   aboutUsDetails: `${siteDetailsBase}/about-us-details`,
   homePageDetails: `${siteDetailsBase}/home-titles`,
+  appLogo: `${siteDetailsBase}/logo`,
 };
